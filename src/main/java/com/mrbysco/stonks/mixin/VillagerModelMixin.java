a892005cc8c1be51f3fbefadbd3ad3ee181f9d7f
Mrbysco <Mrbysco@users.noreply.github.com>
2025-12-03 22:16:49 +0100

    Port to 1.21.10

--- src/main/java/com/mrbysco/stonks/mixin/VillagerModelMixin.java
+++ src/main/java/com/mrbysco/stonks/mixin/VillagerModelMixin.java
@@ -1,31 +1,41 @@
 package com.mrbysco.stonks.mixin;
 
 import com.mrbysco.stonks.client.HeadHelper;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.VillagerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.state.VillagerRenderState;
-import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VillagerModel.class)
-public class VillagerModelMixin<T extends Entity> {
+public abstract class VillagerModelMixin extends EntityModel<VillagerRenderState> {
 
 	@Shadow
 	@Final
 	private ModelPart head;
 
-	@Shadow
-	@Final
+	@Unique
+	private ModelPart hat;
+
+	@Unique
 	private ModelPart hatRim;
 
-	@Shadow
-	@Final
-	private ModelPart hat;
+	protected VillagerModelMixin(ModelPart root) {
+		super(root);
+	}
+
+	@Inject(method = "Lnet/minecraft/client/model/VillagerModel;<init>(Lnet/minecraft/client/model/geom/ModelPart;)V",
+			at = @At("TAIL"))
+	private void stonks$init(ModelPart root, CallbackInfo ci) {
+		this.hat = this.head.getChild("hat");
+		this.hatRim = hat.getChild("hat_rim");
+	}
 
 	@Inject(method = "setupAnim(Lnet/minecraft/client/renderer/entity/state/VillagerRenderState;)V",
 			at = @At("TAIL"))
